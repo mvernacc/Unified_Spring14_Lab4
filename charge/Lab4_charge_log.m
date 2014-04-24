e6f35ce1f8eae10739e6851dfc1bc4161e246e71
Matthew Vernacchia <mvernacc@mit.edu>
2014-04-23 21:01:58 -0400

    charging draft

--- charge/Lab4_charge_log.m
+++ charge/Lab4_charge_log.m
@@ -10,8 +10,9 @@ T = 0.1;
 
 % Analog input pins
 PIN_BAT_I = 0; % Battery current sensor on AIN0
-PIN_BAT_V = 1; % Battery Voltage measured by AIN1
-PIN_THROTTLE = 4; % ESC throttle PWM signal on FIO4
+PIN_BAT_V = 1; % Battery voltage measured by AIN1
+PIN_SOL_I = 2; % Solar panel current sensor on AIN2
+PIN_SOL_V = 3; % Solar panel voltage measured by AIN3
 
 % make LabJack object
 ljasm = NET.addAssembly('LJUDDotNet'); %Make the UD .NET assembly visible in MATLAB
@@ -31,22 +32,6 @@ try
     chanObj = System.Enum.ToObject(chanType, 0); %channel = 0
     ljudObj.ePut(ljhandle, LabJack.LabJackUD.IO.PIN_CONFIGURATION_RESET, chanObj, 0, 0);
     
-    %Set the timer/counter pin offset, which will put the first timer/counter on the throttle pin.
-    ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_CONFIG, LabJack.LabJackUD.CHANNEL.TIMER_COUNTER_PIN_OFFSET, PIN_THROTTLE, 0, 0);
-    %Use the 48 MHz timer clock base with divider (LJ_tc48MHZ_DIV = 26).  Since we are using clock with divisor
-    %support, Counter0 is not available.
-    ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_CONFIG, LabJack.LabJackUD.CHANNEL.TIMER_CLOCK_BASE, 26, 0, 0);
-    
-    %Set the divisor to 14 so the actual timer clock is 3.43 MHz.
-    ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_CONFIG, LabJack.LabJackUD.CHANNEL.TIMER_CLOCK_DIVISOR, 14, 0, 0);
-    %Enable 1 timer.  It will use FIO4.
-    ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_CONFIG, LabJack.LabJackUD.CHANNEL.NUMBER_TIMERS_ENABLED, 1, 0, 0);
-    
-    %Configure Timer0 as 16-bit PWM (LJ_tmPWM16 = 0).  Frequency will be 3.42 MHzHz/(2^16) = 52.3 Hz,
-    % which is close to the 50 Hz frequency for RC aircraft PWM.
-    LJ_tmPWM16 = 0;
-    ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_TIMER_MODE, 0, LJ_tmPWM16, 0, 0);
-    
     % Execute the requests.
     ljudObj.GoOne(ljhandle);
 catch e
@@ -58,28 +43,14 @@ fprintf('done\n');
 date_string = datestr(now);
 date_string = strrep(date_string, ':', '-');
 date_string = strrep(date_string, ' ', '_');
-filename = ['Unified_Lab4_battery_data_', date_string, '.csv'];
+filename = ['Unified_Lab4_charge_data_', date_string, '.csv'];
 fprintf('Opening log file %s ...', filename);
 fileID = fopen(filename, 'W');
 fprintf('done\n');
 
 % Set up the stop box:
 FS = stoploop({'Click here to stop the test'});
-fprintf('Srarting test\n');
-
-% hold the throttle at 0 for several second to get the ESC set up
-fprintf('Zero throttle to start ESC...');
-try
-    %Set the PWM duty cycle.
-    duty = esc_throttle_to_pwm_duty(0);
-    ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_TIMER_VALUE, 0, u3_pwm_duty(duty), 0, 0);
-    % Execute the requests.
-    ljudObj.GoOne(ljhandle);
-catch
-    showErrorMessage(e)
-end
-pause(5);
-fprintf('done\n');
+fprintf('Starting test\n');
 
 % Record test start time
 t0 = clock;
@@ -87,44 +58,52 @@ t0 = clock;
 done = false;
 while ~done && ~FS.Stop()
     t_cycle_start = etime(clock,t0);
-    % Throttle setting for this cycle
-    throttle = throttle_time(t_cycle_start);
-    duty = esc_throttle_to_pwm_duty(throttle);
     % Communicate with the labjack
-    I_value_raw_mean=0; V_value_raw_mean=0;
+    I_bat_value_raw_mean=0; V_bat_value_raw_mean=0;
+    I_sol_value_raw_mean=0; V_sol_value_raw_mean=0;
     try
         n = 16; % number of readings to average
-        I_values_raw = zeros(1,n);
-        V_values_raw = zeros(1,n);
-        %Request a single-ended reading from the current sensor.
+        I_bat_values_raw = zeros(1,n);
+        V_bat_values_raw = zeros(1,n);
+        I_sol_values_raw = zeros(1,n);
+        V_sol_values_raw = zeros(1,n);
+        %Request a single-ended reading from the battery current sensor.
         ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_BAT_I, 0, 0, 0);
         %Request a single-ended reading from the battery voltage pin.
         ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_BAT_V, 0, 0, 0);
-        %Set the PWM duty cycle.
-        ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_TIMER_VALUE, 0, u3_pwm_duty(duty), 0, 0);
+        %Request a single-ended reading from the solar panel current sensor.
+        ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_SOL_I, 0, 0, 0);
+        %Request a single-ended reading from the solar panel voltage pin.
+        ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_SOL_V, 0, 0, 0);
         
         for i = 1:n
             % Execute the requests.
             ljudObj.GoOne(ljhandle);
             
             % Get the result values
-            [ljerror, I_values_raw(i)] = ljudObj.GetResult(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_BAT_I, 0);
-            [ljerror, V_values_raw(i)] = ljudObj.GetResult(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_BAT_V, 0);
+            [ljerror, I_bat_values_raw(i)] = ljudObj.GetResult(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_BAT_I, 0);
+            [ljerror, V_bat_values_raw(i)] = ljudObj.GetResult(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_BAT_V, 0);
+            [ljerror, I_sol_values_raw(i)] = ljudObj.GetResult(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_SOL_I, 0);
+            [ljerror, V_sol_values_raw(i)] = ljudObj.GetResult(ljhandle, LabJack.LabJackUD.IO.GET_AIN, PIN_SOL_V, 0);
         end
-        I_value_raw_mean = mean(I_values_raw);
-        V_value_raw_mean = mean(V_values_raw);
+        I_bat_value_raw_mean = mean(I_bat_values_raw);
+        V_bat_value_raw_mean = mean(V_bat_values_raw);
+        I_sol_value_raw_mean = mean(I_sol_values_raw);
+        V_sol_value_raw_mean = mean(V_sol_values_raw);
     catch e
         showErrorMessage(e)
     end
     % Convert the raw sensor readings to real units
-    I_value = ACS714_convert(I_value_raw_mean);
-    V_value = V_value_raw_mean;
+    I_bat_value = ACS714_convert(I_bat_value_raw_mean);
+    V_bat_value = V_bat_value_raw_mean;
+    I_sol_value = ACS714_convert(I_sol_value_raw_mean);
+    V_sol_value = V_sol_value_raw_mean;
     
     % Print the readings
-    fprintf('Current = %0.2f A, Voltage = %0.2f V\n', I_value, V_value);
+    fprintf('Solar Panel Voltage = %0.2f V, Battery Voltage = %0.2f V\n', V_sol_value, V_bat_value);
     
     % Write to the data log file
-    fprintf(fileID, '%.3f,%.3f,%.3f,%.3f\n', t_cycle_start, I_value, V_value, throttle);
+    fprintf(fileID, '%.3f,%.3f,%.3f,%.3f,%.3f\n', t_cycle_start, I_bat_value, V_bat_value, I_sol_value, V_sol_value);
     
     % Wait until it's time for the next cycle
     t_used = etime(clock,t0) - t_cycle_start;
@@ -132,17 +111,6 @@ while ~done && ~FS.Stop()
     if t_wait > 0
         pause(t_wait);
     end
-end
-
-% turn off the throttle
-fprintf('Turing motor off\n');
-try
-    %Set the PWM duty cycle to 0%.
-    ljudObj.AddRequest(ljhandle, LabJack.LabJackUD.IO.PUT_TIMER_VALUE, 0, u3_pwm_duty(0.0), 0, 0);
-    % Execute the requests.
-    ljudObj.GoOne(ljhandle);
-catch
-    showErrorMessage(e)
 end
 
 % clean up the stop box
--- charge/Lab4_charge_plot.m
+++ charge/Lab4_charge_plot.m
@@ -7,9 +7,9 @@ addpath('..')
 
 %% Battery data
 % Battery maxiumum safe voltage [V]
-bat_v_max = 4.2*2;
+bat_v_max = 4.2;
 % Battery minimum safe voltage [V]
-bat_v_min = 3.5*2;
+bat_v_min = 3.5;
 
 %% Load data from test log
 % filename of .csv file containing the data log
@@ -20,70 +20,84 @@ M = csvread(filename);
 % Time [s]
 t = M(:,1);
 % Battery current [A]
-I_raw = M(:,2);
+I_bat_unfilt = M(:,2);
 % Battery voltage [V]
-V_raw = M(:,3);
-% throttle setting [0 to 1]
-throttle = M(:,4);
+V_bat_unfilt = M(:,3);
+% Solar panel current [A]
+I_sol_unfilt = M(:,4);
+% Solar panel voltage [V]
+V_sol_unfilt = M(:,5);
+
 
 %% Filter the voltage and current
-I = lowPass(I_raw,0.2);
-V = lowPass(V_raw,0.2);
+I_bat = lowPass(I_bat_unfilt,0.2);
+V_bat = lowPass(V_bat_unfilt,0.2);
+I_sol = lowPass(I_sol_unfilt,0.2);
+V_sol = lowPass(V_sol_unfilt,0.2);
 
 %% Integrate the battery charge
 % battery charge level relative to test start [Coulombs]
 c = zeros(size(t));
 for i = 2:length(t)
     dt = t(i)-t(i-1);
-    c(i) = c(i-1) - I(i)*dt;
+    c(i) = c(i-1) + I_bat(i)*dt;
 end
 % battery charge level relative to test start [mA hr]
 c_mahr = c*0.2778;
+
+%% Power
+% Power output of the solar panel [W]
+P_sol = I_sol.*V_sol;
+% Power delivered to the batteries [W]
+P_bat = I_bat.*V_bat;
+
+%% Charger efficiency
+eta = P_bat./P_sol;
 
 %% Plot time traces
 figure('Name', 'Lab4 Time Traces')
 % current
 subplot(4,1,1)
-plot(t,I,'b', t,I_raw,':r')
+plot(t,I_bat,'r', t,I_bat_unfilt,':r', t,I_sol,'b', t,I_sol_unfilt,'b:');
 grid on
 xlabel('Time since test start [s]')
 ylabel('Battery current [A]')
-legend('Filtered', 'Raw')
+legend('Battery, Filtered', 'Battery, Unfiltered', 'Solar Panel, Filtered', 'Solar Panel, Unfiltered')
 % voltage
 subplot(4,1,2)
-plot(t,V,'b', t,V_raw,':r')
+plot(t,V_bat,'r', t,V_bat_unfilt,':r', t,V_sol,'b', t,V_sol_unfilt,'b:');
 grid on
 xlabel('Time since test start [s]')
 ylabel('Battery voltage [V]')
-legend('Filtered', 'Raw')
+legend('Battery, Filtered', 'Battery, Unfiltered', 'Solar Panel, Filtered', 'Solar Panel, Unfiltered')
 % battery charge
 subplot(4,1,3)
 plot(t,c_mahr)
 grid on
 xlabel('Time since test start [s]')
 ylabel('Battery charge [mA hr]')
-% throttle
+% power
 subplot(4,1,4)
-plot(t,throttle)
+plot(t,P_bat,'r', t,P_sol,'b')
 grid on
 xlabel('Time since test start [s]')
-ylabel('Throttle [0 to 1]')
+ylabel('Power [W]')
+legend('Battery', 'Solar Panel')
 
-%% Plot discharge curve
+%% Plot charging curve
 figure('Name', 'Lab4 Battery Dischage Curve')
 hold on
-plot(c_mahr, V)
+plot(c_mahr, V_bat)
 plot(c_mahr, bat_v_max*ones(size(c_mahr)), 'r:')
 plot(c_mahr, bat_v_min*ones(size(c_mahr)), 'r:')
 grid on
 xlabel('Battery charge level, reltive to start [mA hr]')
 ylabel('Battery voltage [V]')
-title('Battery Discharge curve')
+title('Battery Charging Curve')
 
-%% Plot throttle vs power
-figure('Name', 'Power Level vs Throttle Setting')
-plot(throttle, V.*I)
-xlabel('Throttle setting [0 to 1]')
-ylabel('Power drawn from battery, [W]')
-title(sprintf('Electronic Speed Controller\nPower Draw vs Throttle Setting'))
-
+%% Plot charger efficiency
+figure('Name', 'Lab4 Charger Efficiency')
+plot(P_sol, eta)
+xlabel('Power draw from solar panel [W]')
+ylabel('Charger Efficiency [-]')
+title('Charger Efficiency')
